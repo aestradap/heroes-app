de14c1227ce50ad093f5ce7f5f104473294eb27a
Alain Estrada <aestradap17@gmail.com>
2023-10-25 22:20:49 -0400

    heroScreen (useNavigate to go back)

--- src/components/heroes/HeroesScreen.js
+++ src/components/heroes/HeroesScreen.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Navigate, useParams} from "react-router-dom";
+import {Navigate, useNavigate, useParams} from "react-router-dom";
 import {getHeroesById} from "../../selectors/getHeroById";
 
 
 export const HeroesScreen = () => {
 
     const {heroeId} = useParams();
+    const navigate = useNavigate();
 
     const hero = getHeroesById(heroeId);
     if (!hero) {
@@ -13,16 +14,51 @@ export const HeroesScreen = () => {
     }
 
     const {
-        id,
         superhero,
         alter_ego,
+        publisher,
         first_appearance,
         characters
     } = hero;
 
+    const handleReturn = () => {
+        // navigate(-1)   // v5's history.go(-1) or history.goBack()
+        // navigate(1)    // v5's history.go(1) or history.goForward()
+        // navigate(2)    // v5's history.go(2)
+        // https://blog.logrocket.com/migrating-react-router-v6-guide/
+        return navigate(-1);
+    }
+
     return <>
 
-        <div>Hero Screen</div>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img
+                    src={`../assets/heroes/${heroeId}.jpg`}
+                    alt={superhero}
+                    className="img-thumbnail"
+                />
+            </div>
+            <div className="col-8">
+                <h3>{superhero}</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><b>Alter ego: </b>{alter_ego}</li>
+                    <li className="list-group-item"><b>Publisher: </b>{publisher}</li>
+                    <li className="list-group-item"><b>First appearance:</b>{first_appearance}</li>
+                </ul>
+
+                <h5>Characters</h5>
+                <p>{characters}</p>
+
+                <button
+                    className="btn btn-outline-info"
+                    onClick={handleReturn}
+                >
+                    Return
+                </button>
+
+            </div>
+        </div>
 
     </>
 
